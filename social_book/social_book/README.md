02bdb9a0bf2413d3bf89115cfba1decc90229713
tomasotano25 <tomasotano25@gmail.com>
2022-05-23 19:20:34 -0400

    configurar media

---
+++ social_book/social_book/README.md
@@ -0,0 +1,19 @@
+# Inicial un projecto y una app en django
+
+```
+django-admin startproject social_book
+
+django-admin startapp core
+```
+
+## Ejecutar el servidor
+
+```
+python manage.py runserver
+```
+
+## Rutas de acceso
+
+```
+
+```
---
+++ social_book/social_book/core/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Profile(models.Model):
+    user = pass
+    id_iser = pass
+    bio = pass
+    profileimg = models.ImageField(upload_to='profile_images')
+    location = pass
