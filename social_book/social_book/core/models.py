7481aff1f7a9e1686702a730b4089408269639c9
tomasotano25 <tomasotano25@gmail.com>
2022-05-25 11:01:56 -0400

    agregar signup

--- social_book/social_book/core/models.py
+++ social_book/social_book/core/models.py
@@ -4,12 +4,11 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 # Create your models here.
-
-
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    id_iser = models.IntegerField()
+    id_user = models.IntegerField()
     bio = models.TextField(blank=True)
     profileimg = models.ImageField(
         upload_to='profile_images', default='blank-profile-picture.png')  # blank-profile-picture.png
--- social_book/social_book/core/views.py
+++ social_book/social_book/core/views.py
@@ -1,3 +1,4 @@
+from .models import Profile
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
@@ -29,8 +30,24 @@ def signup(request):
                 user = User.objects.create_user(
                     username=username, email=email, password=password)
                 user.save()
+
+                # Log user in and redirect to settings page
+
+                # create a Profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(
+                    user=user_model, id_user=user_model.id)
+                new_profile.save()
+
+                return redirect('signup')
+
         else:
             messages.info(request, 'Password Not Matching')
             return redirect('signup')
     else:
         return render(request, 'signup.html')
+
+
+def signin(request):
+
+    return render(request, 'signin.html')
